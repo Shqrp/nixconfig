f6b720b95f66292f5ec23e42f4d1d1c5fddde096
Shqrp <shqrp@shqrp.lol>
2024-07-26 16:00:48 +0200

    add hyprland build cache

--- system.nix
+++ system.nix
@@ -18,10 +18,14 @@
   };
 
   nix = {
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
     gc = {
       automatic = true;
       options = "--delete-older-than 14d";
